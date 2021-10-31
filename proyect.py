d8dc7aee559357f3829db8508d6da6bac8ccdb66
Giovanni-Boris <bancasi@unsa.edu.pe>
2021-10-30 23:59:44 -0500

    Mostrado de imagenes en camara para visualiar mi rostro

--- proyect.py
+++ proyect.py
@@ -1,4 +1,18 @@
 import cv2
 
 #Iniciamos camara
-vc = cv2.VideoCapture(0);
+vc = cv2.VideoCapture(0)
+
+while True:
+  #Capturamos video frame a frame
+  ret, frame = vc.read()
+  if ret==False:
+    break
+
+  cv2.imshow('Video', frame)
+  if  cv2.waitKey(1) & 0xFF==ord("q"):
+    break;
+#Finalizamos camarai,grabacion y cerramos ventana
+vc.release()
+cv2.destroyAllWindows()
+
