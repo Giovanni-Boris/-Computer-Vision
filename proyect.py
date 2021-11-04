2e110b1d32916f8c36d0db1307bcfdcac27072b4
Giovanni-Boris <bancasi@unsa.edu.pe>
2021-11-04 15:59:17 -0500

    Dibujando el areas y punto central de los objetos

--- proyect.py
+++ proyect.py
@@ -29,7 +29,16 @@ while True:
     for i in contornos:
       area=cv2.contourArea(i)
       if area>3000:
+        M=cv2.moments(i)
+        if(M["m00"]==0): M["m00"]=1
+        x=int(M["m10"]/M["m00"])
+        y=int(M["m01"]/M["m00"])
+        cv2.circle(frame,(x,y),7,(0,255,0),-1)
+        font=cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame,'{},{}'.format(x,y),(x+10,y),font,0.75,
+            (0,225,0),1,cv2.LINE_AA)
         nuevoContorno=cv2.convexHull(i)
+
         cv2.drawContours(frame,[nuevoContorno],0,(125,0,0),3)
     #En la imagen frameHSV vamos a encontrar los rango bajo01 y alto 1 los mismo para 2
     #firstRed1=cv2.inRange(frameHSV,redBajo01,redAlto01)
