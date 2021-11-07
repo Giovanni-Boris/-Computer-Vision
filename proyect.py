1b29896206196176e960ccb19b566f5ecfdad7c3
Giovanni-Boris <bancasi@unsa.edu.pe>
2021-11-06 19:02:05 -0500

    Transformaciones morfaclogicas y suavizacion de color

--- proyect.py
+++ proyect.py
@@ -5,7 +5,7 @@ import numpy as np
 #Iniciamos camara
 vc = cv2.VideoCapture(0)
 #Marcador 
-azulBajo02 = np.array([100,100,20],np.uint8)   
+azulBajo02 = np.array([110,100,20],np.uint8)   
 azulAlto02 = np.array([125,255,255],np.uint8) 
 #Colores para pintar
 verde = (0,255,36)
@@ -55,13 +55,18 @@ while True:
     cv2.circle(frame,(1245,80),7,(0,0,0),-1)
     cv2.rectangle(frame,(1220,110),(1270,150),(0,0,0),big)
     cv2.circle(frame,(1245,130),11,(0,0,0),-1)
-
-
-
+    #Deteccion de celeste
     mask = cv2.inRange(frameHSV,azulBajo02,azulAlto02)
+    #Transformacione morfologicas para mejorar imagen binaria
+    mask = cv2.erode(mask,None,iterations=1)
+    mask = cv2.dilate(mask,None,iterations=2)
+    #Suavizando colores
+    mask = cv2.medianBlur(mask,13)
     #Deteccion de contornos
     contornos,_ = cv2.findContours(mask,cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
+    #Contorno mas grande segun su area
+    contornos = sorted(contornos,key=cv2.contourArea,reverse=True)[:1]
     #Dibujar las contornos
     #cv2.drawContours (frame, contornos, -1, (255,0,0), 3)
     #Eliminado algunos contornos no deseados
@@ -79,19 +84,9 @@ while True:
         nuevoContorno=cv2.convexHull(i)
 
         cv2.drawContours(frame,[nuevoContorno],0,(125,0,0),3)
-    #En la imagen frameHSV vamos a encontrar los rango bajo01 y alto 1 los mismo para 2
-    #firstRed1=cv2.inRange(frameHSV,redBajo01,redAlto01)
-    #secondRed2=cv2.inRange(frameHSV,redBajo02,redAlto02)
-    #Adicionar las dos para convertirla en una solo y me detecte el rojo
-    #unity=cv2.add(firstRed1,secondRed2)
-    #En vez de blanco mostrar el color real 
-    #unityOriginal=cv2.bitwise_and(frame,frame,mask=unity)
-    #cv2.imshow("unityOriginal",unityOriginal)
-    #Visualiazacion detección de los colores
-    #cv2.imshow("Mascara",mask)
-    #Mostramos el frame capturado
     cv2.imshow('Video', frame)
-    cv2.imshow('aux', aux)
+    #cv2.imshow('aux', aux)
+    cv2.imshow("azul",mask)
 
     #Si pulsamos q finalizamos'
     if  cv2.waitKey(1) & 0xFF==ord("q"):
